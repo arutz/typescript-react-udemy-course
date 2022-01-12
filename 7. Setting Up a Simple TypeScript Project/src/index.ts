2fdb5e6b72285a2153d7af3038220cb9ea087a10
Achim Rutz <a.rutz@communicationX.de>
2022-01-12 15:45:03 +0100

    chapter 08 done

--- 7. Setting Up a Simple TypeScript Project/src/index.ts
+++ 7. Setting Up a Simple TypeScript Project/src/index.ts
@@ -1,8 +1,7 @@
-function multiply(a: number, b: number) : number {
-    return a * b;
-}
+import multiply, {multiplyBy2, HelloWorld} from "./multiply";
 
 const a = 5;
 const b = 2;
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
+console.log(`${b} * 2 = ${multiplyBy2(b)}`); 
---
+++ 7. Setting Up a Simple TypeScript Project/src/multiply.ts
@@ -0,0 +1,11 @@
+export default function multiply(a: number, b: number) : number {
+    return a * b;
+}
+
+export function multiplyBy2(num: number) {
+    return num * 2;
+}
+
+export interface HelloWorld {
+    
+}
